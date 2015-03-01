e1f07819fdff563b23910593f9cab7f7b82d2019
Nick Strayer <nstrayer@uvm.edu>
2015-03-01 17:07:18 -0500

    Added a side by side histogram of FDR to Pvalues

--- work.r
+++ work.r
@@ -1,16 +1,34 @@
 setwd("/Users/Nick/spring15/independentStudy")
 #A point of note: fdrtool was written under R 3.1.2 which is the latest. 
-#R.Version() #A quick way to check if you need. 
+#R.Version() #A quick way to check if you need.
+
+# Incase you need to isntall the packages
+# install.packages("fdrtool")
+# install.packages("ggplot2")
+# install.packages("gridExtra")
 
 require(fdrtool)
 require(ggplot2)
+library(gridExtra)
 
-obesity = read.table("data/obesity_censored.txt", header = TRUE, fill = TRUE)
+
+obesityRaw = read.table("data/obesity_censored.txt", header = TRUE, fill = TRUE)
+obesity = na.omit(obesityRaw) #There are some NAs in the data.
 
 m = ggplot(obesity, aes(x = pvalue)) + 
-  geom_histogram(aes(y=..count../sum(..count..)), color = "white", fill = "black") +
-  labs(title = "Histogram of p-values for obesity Words (normalized)",
+  geom_histogram(aes(y=..count../sum(..count..)), color = "white", fill = "black") + 
+  labs(title = "Histogram of p-values for obesity words (normalized)",
       x = "P-Value", y = "% of words at p-val")
 m #Have to actually call the ggplot variable to see it
+ggsave("figures/Obesity_PVal_Hist_Norm.pdf") #Save
 
-ggsave("figures/Testing.pdf") #Save
+
+FDRresults = fdrtool(obesity$pvalue, statistic = "pvalue", plot = TRUE)
+obesity$qvalue = FDRresults$qval
+
+#Plot the 
+P   = ggplot(obesity, aes(x = pvalue)) + geom_histogram(aes(y=..count../sum(..count..)),fill = "#7fc97f", alpha = 0.6) + ylim(0,0.4)
+Q   = ggplot(obesity, aes(x = qvalue)) + geom_histogram(aes(y=..count../sum(..count..)), fill = "#beaed4", alpha = 0.6) + ylim(0,0.4)
+out = arrangeGrob(P,Q,ncol=2, main = "P-Val vs, Q-Val")
+out #just so we can see the output. 
+ggsave("figures/Obesity_PVal_vs_Qval.pdf",out) #Save
